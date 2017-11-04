8b7e242f1757a1673b7437f7a33b0aa5e891cd45
小星 <753940262@qq.com>
2017-11-04 12:55:08 +0800

    1、添加gif1

--- gifloadingview/src/main/java/com/xiaoxing/gifloadingview/LoadingDialogUtil.java
+++ gifloadingview/src/main/java/com/xiaoxing/gifloadingview/LoadingDialogUtil.java
@@ -12,6 +12,7 @@ public class LoadingDialogUtil {
 
 
     private static GifLoadingView mGifLoadingView;
+    private static GifLoadingView1 mGifLoadingView2;
 
 
     public static void showGifdialog1(FragmentManager fragmentManager, int intRes) {
@@ -22,9 +23,20 @@ public class LoadingDialogUtil {
 
     }
 
+    public static void showGifdialog2(FragmentManager fragmentManager, int intRes) {
+
+        mGifLoadingView2 = new GifLoadingView1();
+        mGifLoadingView2.setImageResource(intRes);
+        mGifLoadingView2.show(fragmentManager, "");
+
+    }
+
     public static void dismissDialog() {
         if (mGifLoadingView != null) {
             mGifLoadingView.dismiss();
+        }
+        if (mGifLoadingView2 != null) {
+            mGifLoadingView2.dismiss();
         }
     }
 
