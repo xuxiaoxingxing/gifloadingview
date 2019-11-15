2159d15b0277736620eb1e7c392cf24040c488c6
小星 <753940262@qq.com>
2019-11-15 17:29:09 +0800

    1、bug调整

--- gifloadingview/src/main/java/com/xiaoxing/gifloadingview/GifLoadingView1.java
+++ gifloadingview/src/main/java/com/xiaoxing/gifloadingview/GifLoadingView1.java
@@ -1,13 +1,16 @@
 package com.xiaoxing.gifloadingview;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.view.Gravity;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -16,6 +19,7 @@ import android.widget.ImageView;
  * Created by Roger on 2016/4/28.
  */
 public class GifLoadingView1 extends DialogFragment {
+    private static final String SAVED_DIALOG_STATE_TAG = "android:savedDialogState";
     private static GifLoadingView1 sInstance = null;
     private Dialog mDialog;
     //  private GradientDrawable gd;
@@ -126,6 +130,34 @@ public class GifLoadingView1 extends DialogFragment {
     public void onDismiss(DialogInterface dialog) {
         super.onDismiss(dialog);
         mDialog = null;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        if (getShowsDialog()) {
+            setShowsDialog(false);
+        }
+        super.onActivityCreated(savedInstanceState);
+        setShowsDialog(true);
+
+        View view = getView();
+        if (view != null) {
+            if (view.getParent() != null) {
+                throw new IllegalStateException(
+                        "DialogFragment can not be attached to a container view");
+            }
+            getDialog().setContentView(view);
+        }
+        final Activity activity = getActivity();
+        if (activity != null) {
+            getDialog().setOwnerActivity(activity);
+        }
+        if (savedInstanceState != null) {
+            Bundle dialogState = savedInstanceState.getBundle(SAVED_DIALOG_STATE_TAG);
+            if (dialogState != null) {
+                getDialog().onRestoreInstanceState(dialogState);
+            }
+        }
     }
 
     public void setBackgroundResource(int id) {
